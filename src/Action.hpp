1b182a25a9342caea72665497fc5344700930e3e
Simon Chen <simonc8192@gmail.com>
2025-05-19 00:41:16 +0800

    Action component

---
+++ src/Action.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+class Action
+{
+	std::string m_name = "NONE";
+	std::string m_type = "NONE";
+
+public:
+	Action() = default;
+	Action(const std::string& name, const std::string& type)
+		: m_name(name), m_type(type) { }
+
+	const std::string& name() const
+	{
+		return m_name;
+	}
+	const std::string& type() const
+	{
+		return m_type;
+	}
+};
